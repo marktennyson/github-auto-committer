faccafd7a6dc4da106087c46cadb807b140fd6f5
Aniket Sarkar <aniketsarkar1998@gmail.com>
2021-10-21 10:51:11 +0530

    rand commit: 0a386094-9742-441c-aef4-07c5596d4222

--- script.py
+++ script.py
@@ -6,11 +6,16 @@ import click as c
 from datetime import datetime as dt
 
 BASEDIR = os.path.abspath(os.curdir)
-RANGE_COUNT = 20
+RANGE_COUNT = 2
 
 def change_file_data():
     with open(os.path.join(BASEDIR, 'file.txt'), 'a') as f:
         f.write(str(uuid.uuid4())+"\n")
+
+def add_file_to_track():
+    resp = os.popen("git add .")
+    data = resp.read()
+    c.secho(str(data), fg="blue")
 
 def make_commit():
     resp = os.popen(f'git commit -m "rand commit: {uuid.uuid4()}"')
@@ -20,7 +25,7 @@ def make_commit():
 def make_push():
     resp = os.popen('git push')
     data = resp.read()
-    c.secho(str(data), fg="green")
+    c.secho(str(data), fg="yellow")
 
 @c.command()
 @c.option('-c', '--count', default=RANGE_COUNT, help='Provide the range count for the total commit')
@@ -29,6 +34,7 @@ def main(count):
         c.secho(f"Process started at: {dt.now()}")
         c.secho(f"Performing the commit count: {i+1}")
         change_file_data()
+        add_file_to_track()
         make_commit()
         make_push()
         c.secho(f"Process completed at: {dt.now()}")
